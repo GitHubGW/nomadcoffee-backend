375f907391c3fd1938c651947eab4c1268c9816b
GW <kowonp@gmail.com>
2022-04-26 15:59:11 +0900

    see coffee shops offset

--- src/coffee/seeCoffeeShops/seeCoffeeShops.resolvers.ts
+++ src/coffee/seeCoffeeShops/seeCoffeeShops.resolvers.ts
@@ -1,31 +1,20 @@
 import { CoffeeShop } from ".prisma/client";
-import { CommonResult } from "../../common/common.interfaces";
 import { Context, Resolvers } from "../../types";
-
-interface SeeCoffeeShopsArgs {
-  page?: number;
-}
-
-interface SeeCoffeeShopsResult extends CommonResult {
-  coffeeShops: CoffeeShop[];
-  totalCoffeeShops: number;
-}
 
 const resolvers: Resolvers = {
   Query: {
-    seeCoffeeShops: async (_: any, { page = 1 }: SeeCoffeeShopsArgs, { prisma }: Context): Promise<SeeCoffeeShopsResult> => {
+    seeCoffeeShops: async (_: any, { offset }: any, { prisma }: Context) => {
       try {
         const foundCoffeeShops: CoffeeShop[] = await prisma.coffeeShop.findMany({
-          skip: (page - 1) * 6,
           take: 6,
+          skip: offset,
           include: { categories: true, user: true, coffeeShopPhotos: true },
           orderBy: { createdAt: "desc" },
         });
-        const countedCoffeeShops: number = await prisma.coffeeShop.count();
-        return { ok: true, message: "전체 커피숍 보기에 성공하였습니다.", coffeeShops: foundCoffeeShops, totalCoffeeShops: countedCoffeeShops };
+        return foundCoffeeShops;
       } catch (error) {
         console.log("seeCoffeeShops error");
-        return { ok: false, message: "전체 커피숍 보기에 실패하였습니다.", coffeeShops: [], totalCoffeeShops: 0 };
+        return [];
       }
     },
   },
--- src/coffee/seeCoffeeShops/seeCoffeeShops.typeDefs.ts
+++ src/coffee/seeCoffeeShops/seeCoffeeShops.typeDefs.ts
@@ -1,14 +1,7 @@
 import { gql } from "apollo-server-core";
 
 export default gql`
-  type SeeCoffeeShopsResult {
-    ok: Boolean!
-    message: String!
-    coffeeShops: [CoffeeShop]
-    totalCoffeeShops: Int!
-  }
-
   type Query {
-    seeCoffeeShops(page: Int): SeeCoffeeShopsResult!
+    seeCoffeeShops(offset: Int!): [CoffeeShop]
   }
 `;
